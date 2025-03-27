6523daad1eddb3558eb21b7fa6cb63b9eeb3eabb
sirius54s <tu.email@ejemplo.com>
2025-03-27 01:03:01 -0300

    test 2

--- ryh-admin/src/layouts/MainLayout.vue
+++ ryh-admin/src/layouts/MainLayout.vue
@@ -2,7 +2,7 @@
   <q-layout view="lHh Lpr lFf">
     <q-header elevated>
       <q-toolbar class="bg-white text-black">
-        <q-toolbar-title> RYH v{{ $q.version }} </q-toolbar-title>
+        <q-toolbar-title> GestionFix v{{ $q.version }} </q-toolbar-title>
         <q-btn flat dense round icon="settings" aria-label="Menu" />
         <q-btn flat dense round icon="search" aria-label="Search" />
         <q-btn flat dense round icon="more_vert" aria-label="More" />
