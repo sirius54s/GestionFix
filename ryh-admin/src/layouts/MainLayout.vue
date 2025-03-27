1b84514c483227982bebc94c9a2fc1adcdcf2252
sirius54s <tu.email@ejemplo.com>
2025-03-26 21:40:34 -0300

    un nuevo comienzo

---
+++ ryh-admin/src/layouts/MainLayout.vue
@@ -0,0 +1,17 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar class="bg-white text-black">
+        <q-toolbar-title> RYH v{{ $q.version }} </q-toolbar-title>
+        <q-btn flat dense round icon="settings" aria-label="Menu" />
+        <q-btn flat dense round icon="search" aria-label="Search" />
+        <q-btn flat dense round icon="more_vert" aria-label="More" />
+      </q-toolbar>
+    </q-header>
+    <q-page-container class="bg-white">
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script setup lang="ts"></script>
---
+++ ryh-admin/src/pages/IndexPage.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <indexFunctionModal />
+    <RepairTable />
+  </div>
+</template>
+
+<script setup lang="ts">
+import indexFunctionModal from 'src/components/indexFunctionModal.vue';
+import RepairTable from 'src/components/indexTableRows.vue';
+</script>
